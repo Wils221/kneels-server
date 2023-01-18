2b4f382353596fd3551da6b49d4c657e762844d3
Wil Hammond <wil.hammond@gmail.com>
2023-01-18 13:32:30 -0600

    adds models

---
+++ models/orders.py
@@ -0,0 +1,8 @@
+class Order():
+
+    def __init__(self, id, metal_id, size_id, style_id, timestamp):
+        self.id = id
+        self.metal_id = metal_id
+        self.size_id = size_id
+        self.style_id = style_id
+        self.timestamp = timestamp
---
+++ models/sizes.py
@@ -0,0 +1,6 @@
+class Size():
+
+    def __init__(self, id, caretes, price):
+        self.id = id
+        self.caretes = caretes
+        self.price = price
